222208411f7ca1c41bcfdbb1bc32284e58fad76e
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-09-21 14:03:29 -0400

    Include program that says your name and age

--- Chapter1/main.js
+++ Chapter1/main.js
@@ -1,5 +1,11 @@
-console.log('Hello from JavaScript')
-console.log('Mathematical!')
-console.log(5*9634)
-console.log(5/3)
-console.log('Adios muchacho')
+// console.log('Hello from JavaScript')
+// console.log('Mathematical!')
+// console.log(5*9634)
+// console.log(12/0)
+// console.log('Adios muchacho')
+
+//program that displays name & age
+function coder(name, age) {
+  console.log('Hello my name is ' + name + ' and I am almost ' + age + ' years old')
+}
+coder("Inés", 30)
