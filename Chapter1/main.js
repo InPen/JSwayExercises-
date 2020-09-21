70b8b6d05aa84cc06ab8feb8d314c712734bd65f
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-09-21 14:34:36 -0400

    Write a program that displays the results ofadding, subtracting, multiplying and dividing 6 by 3.

--- Chapter1/main.js
+++ Chapter1/main.js
@@ -5,7 +5,29 @@
 // console.log('Adios muchacho')
 
 //program that displays name & age
-function coder(name, age) {
-  console.log('Hello my name is ' + name + ' and I am almost ' + age + ' years old')
-}
-coder("Inés", 30)
+// function coder(name, age) {
+//   console.log('Hello my name is ' + name + ' and I am almost ' + age + ' years old')
+// }
+// coder("Inés", 30)
+
+// Write a program that displays the results of
+// adding, subtracting, multiplying and dividing 6 by 3.
+  function add(num1, num2) {
+    console.log(num1 + num2)
+  }
+  add(6, 3)
+
+  function sub(num1, num2) {
+    console.log(num1 - num2)
+  }
+  sub(6, 3)
+
+  function multiply(num1, num2) {
+    console.log(num1 * num2)
+  }
+  multiply(6, 3)
+
+  function divide(num1, num2) {
+    console.log(num1/num2)
+  }
+  divide(6,3)
