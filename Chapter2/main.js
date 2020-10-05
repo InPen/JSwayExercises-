17e5f79b6340dce13c622d333b37183f8a56b72c
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-05 14:20:19 -0400

    Template literals practice

---
+++ Chapter2/main.js
@@ -0,0 +1,12 @@
+// const country = "France";
+// console.log(`I live in ${country}`); // Show "I live in France"
+// const x = 3;
+// const y = 7;
+// console.log(`${x} + ${y} = ${x + y}`); // Show "3 + 7 = 10"
+
+const dog = 'Snooky'
+const age = 9
+console.log(`My dog's name is ${dog} and she's ${age} years old`);
+
+const name = ['Johann', 'Han','Mariné', 'Snooky']
+console.log(`Hi, my name is ${name[1]}. How are you?`);
