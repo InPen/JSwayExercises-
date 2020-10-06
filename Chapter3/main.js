437e7c0bee6aa0594d7c63a3b9acb4af850feee3
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-06 12:03:10 -0400

    Switch case

--- Chapter3/main.js
+++ Chapter3/main.js
@@ -3,15 +3,26 @@
 // if (number >= 0) {
 //   console.log(`${number} is positive or zero`);
 // }
-const weather = prompt("What's the weather like?");
-if (weather === "sunny") {
-  console.log("T-shirt time!");
-} else if (weather === "windy") {
-  console.log("Windbreaker life.");
-} else if (weather === "rainy") {
-  console.log("Bring that umbrella!");
-} else if (weather === "snowy") {
-  console.log("Just stay inside!");
-} else {
-  console.log("Not a valid weather type");
-}
+
+// const weather = prompt("What's the weather like?");
+// if (weather === "sunny") {
+//   console.log("T-shirt time!");
+// } else if (weather === "windy") {
+//   console.log("Windbreaker life.");
+// } else if (weather === "rainy") {
+//   console.log("Bring that umbrella!");
+// } else if (weather === "snowy") {
+//   console.log("Just stay inside!");
+// } else {
+//   console.log("Not a valid weather type");
+// }
+
+// const x = "abc";
+// switch (x) {
+//   case "abc":
+//     console.log("x = abc");
+//   // break omitted: the next block is also run!
+//   case "def":
+//     console.log("x = def");
+//     break;
+// }
