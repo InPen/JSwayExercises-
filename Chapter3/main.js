cf0ed7ca211ab270e43da8449dc109e02e76cbc9
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-06 11:33:15 -0400

    Include && and multiple comparisons

--- Chapter3/main.js
+++ Chapter3/main.js
@@ -1,5 +1,17 @@
 //CONDITIONALS
-const number = Number(prompt("Enter a number:"));
-if (number >= 0) {
-  console.log(`${number} is positive or zero`);
+// const number = Number(prompt("Enter a number:"));
+// if (number >= 0) {
+//   console.log(`${number} is positive or zero`);
+// }
+const weather = prompt("What's the weather like?");
+if (weather === "sunny") {
+  console.log("T-shirt time!");
+} else if (weather === "windy") {
+  console.log("Windbreaker life.");
+} else if (weather === "rainy") {
+  console.log("Bring that umbrella!");
+} else if (weather === "snowy") {
+  console.log("Just stay inside!");
+} else {
+  console.log("Not a valid weather type");
 }
