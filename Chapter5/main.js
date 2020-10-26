8a1a13274654683de8af6ed20f678e71605a221a
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-26 16:47:49 -0400

    Add loop

--- Chapter5/main.js
+++ Chapter5/main.js
@@ -7,3 +7,19 @@ console.log(person('Snooky', 9))
 //simplify function using fat arrow
 const sayHello = (firstName, lastName) => `Hello, ${firstName} ${lastName}!`
 sayHello('Johann','Folta')
+
+//Complete the following program so that the square1() and square2() functions work properly.
+// Square the given number x
+const square1 = number => let(i = 0; i <= 10; i++){
+  Math.sqrt(number)
+}
+// Square the given number x
+// const square2 = x => Math.sqrt(x)
+//
+// console.log(square1(0)); // Must show 0
+// console.log(square1(2)); // Must show 4
+// console.log(square1(5)); // Must show 25
+//
+// console.log(square2(0)); // Must show 0
+// console.log(square2(2)); // Must show 4
+// console.log(square2(5)); // Must show 25
