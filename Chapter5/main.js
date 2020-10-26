e01f4ea5e24d4e2c2044a7b242549a15e69b8d0b
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-26 16:24:09 -0400

    Practice simplifying functions using fat arrow

---
+++ Chapter5/main.js
@@ -0,0 +1,9 @@
+const hello = name => `Hello ${name}, you beautiful baby`
+console.log(hello('Han'))
+
+const person = (name,age) => `Your name is ${name} and you're ${age} years old`
+console.log(person('Snooky', 9))
+
+//simplify function using fat arrow
+const sayHello = (firstName, lastName) => `Hello, ${firstName} ${lastName}!`
+sayHello('Johann','Folta')
