5ae232cca2376d92ea3572012993914d79ad6543
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-27 11:01:34 -0400

    Submit chapter exercises

--- Chapter6/main.js
+++ Chapter6/main.js
@@ -14,9 +14,9 @@ const serena = {
   health:100,
   xp:50,
   weapon:'Moon Crystal',
-  function describe(){
-    return('Serena aka Sailor Moon is the princess of the Moon Kingdon ')
-  }
+  describe(){
+    return(`${this.name} aka Sailor Moon is the princess of the Moon Kingdon her health is ${this.health} and her weapon is ${this.weapon} `)
+}
 }
 // serena is harmed by an arrow
 serena.health -= 20;
@@ -28,3 +28,30 @@ serena.strength += 10;
 serena.xp += 15;
 
 console.log(serena.describe());
+
+
+//Complete the following program to add the dog object definition.
+
+const dog = {
+  name:'Snooky',
+  species:'Victorian Bulldog',
+  size:'XLovable',
+  bark(){
+    return `woof woof`
+  }
+}
+
+console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
+
+
+//BANK ACCOUNT
+const bankAccount = {
+  name:'Han Folta',
+  balance:89000000,
+  credit:564000000,
+  describe(){
+    return `${this.name} your balance is $ ${this.balance}`
+  }
+}
+console.log(bankAccount.describe());
